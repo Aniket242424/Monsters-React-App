a34b73e1c1522a3fd3d21a72d52d4a285ab33c24
Aniket242424 <amahangade24@gmail.com>
2020-04-15 11:45:58 +0530

    Updates

---
+++ precache-manifest.6a016b302ea805e317c9acd70819107c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bb21f0cb6fa90df5777f818be6649e",
+    "url": "/Monsters-React-App/index.html"
+  },
+  {
+    "revision": "2235e9d3749d15e7e200",
+    "url": "/Monsters-React-App/static/css/main.7bbdf4cd.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/Monsters-React-App/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-React-App/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2235e9d3749d15e7e200",
+    "url": "/Monsters-React-App/static/js/main.74a0af24.chunk.js"
+  },
+  {
+    "revision": "010911afc5d17962b041",
+    "url": "/Monsters-React-App/static/js/runtime-main.3d13c0db.js"
+  }
+]);
